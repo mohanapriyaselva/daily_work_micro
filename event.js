39e1a3ad8d83c6f3b686e3bf999071fea79f1f6c
SAPNA080402 <sapnasivagnanam@gmail.com>
2023-10-27 17:42:13 +0530

    On change task commit

---
+++ event.js
@@ -0,0 +1,20 @@
+document.getElementById("content_center").className = "d-flex justify-content-center align-items-center";
+function select_event() {
+    var num_1 = document.getElementById("num").value;
+    var opt;
+    for (var i = 1; i <= num_1; i++) {
+        opt += `<option> ${i} </option>`;
+    }
+    document.getElementById("print").innerHTML = opt;
+}
+function box() {
+    var num_1 = document.getElementById("print").value;
+    document.getElementById("box_1").rows = 10;
+    document.getElementById("box_1").columns = 50;
+    var drop = "\t ";
+    for (var i = 1; i <= num_1; i++) {
+        drop += `${i}
+         `;
+    }
+    document.getElementById("box_1").innerHTML = drop;
+}
